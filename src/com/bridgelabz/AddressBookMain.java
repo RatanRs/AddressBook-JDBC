d36d80d92e1201f5bf6be0f193cc1c3d815a04f0
9663461085 <ratansollapure16310@gmail.com>
2023-01-16 15:56:01 +0530

    Added:Retriviwed data from between range

--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -1,4 +1,5 @@
 package com.bridgelabz;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Scanner;
 /***
@@ -13,15 +14,20 @@ public class AddressBookMain {
 
 		boolean exit = false;
 		while (!exit) {
-			System.out.println(" Press\n 1 ->  Retrieve Data\n 2 -> Update Data\n 3 -> Exit");
-			switch (scanner.nextInt()) {
+			System.out.println(" Press\n 1 ->  Retrieve data\n 2 -> Update Address,city,state,zip  by srNo\n "
+					+ "3 Retrieve data for particular date" + "-> \n 4 -> exit");
+			int choice = scanner.nextInt();
+			switch (choice) {
 			case 1:
 				retrieveData();
 				break;
 			case 2:
-				updateCity();
+				update();
 				break;
 			case 3:
+				reteriveDataForParticularDate();
+				break;
+			case 4:
 				exit = true;
 			}
 		}
@@ -34,9 +40,21 @@ public class AddressBookMain {
 			System.out.println(employee + "\n");
 		}
 	}
-	private static void updateCity() {
-        AddressBook addressBookRepo = new AddressBook();
-        System.out.println("Enter the address,city,state, zip and id  to Update");
-        addressBookRepo.updateCityByZip(scanner.next(), scanner.next(), scanner.next(), scanner.nextInt(), scanner.nextInt());
+
+	private static void update() {
+		AddressBook addressBookRepo = new AddressBook();
+		System.out.println("Enter the address,city,state, zip and id  to Update");
+		addressBookRepo.updateCityByZip(scanner.next(), scanner.next(), scanner.next(), scanner.nextInt(),
+				scanner.nextInt());
+	}
+	private static void reteriveDataForParticularDate() {
+        AddressBook addressBook = new AddressBook();
+        System.out.println("Enter the Date of Joining (YYYY-MM-DD");
+        System.out.println("Enter year , month and Day ex: 2020 02 03");
+        List<ContactPerson> employeeInfoList = addressBook.findAllForParticularDate(LocalDate.of(scanner.nextInt(), scanner.nextInt(), scanner.nextInt()));
+        for (ContactPerson employee : employeeInfoList)
+         {
+            System.out.println(employee + "\n");
+        }
     }
 }
